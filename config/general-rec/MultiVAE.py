acdf17de512c04abedf7cd57e65909d3d706a21a
임도현_T5170 <86715604+limstonestone@users.noreply.github.com>
2023-06-06 02:42:22 +0900

    feat : add Multi-VAE configs
    * feat : change setting for wandb (issue-#7)
    
    * feat : add only test metric
    
    * feat : add MultiVAE configs
    
    - '#3'

--- config/general-rec/MultiVAE.py
+++ config/general-rec/MultiVAE.py
@@ -17,3 +17,106 @@ class Ver0(BaseConfig):
             },
         }
         self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_1(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 100,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "latent_dimendion": [64],  # 128 -> 64
+            "mlp_hidden_size": [200],  # 600 -> 200
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_2(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 100,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "latent_dimendion": [64],  # 128 -> 64
+            "mlp_hidden_size": [200],  # 600 -> 200
+            "learning_rate": 0.005,  # 0.001 -> 0.005
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_3(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 100,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "latent_dimendion": [64],  # 128 -> 64
+            "mlp_hidden_size": [200],  # 600 -> 200
+            "learning_rate": 0.0005,  # 0.001 -> 0.0005
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_4(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 300,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "latent_dimendion": [64],  # 128 -> 64
+            "mlp_hidden_size": [200],  # 600 -> 200
+            "learning_rate": 0.0005,  # 0.001 -> 0.0005
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_5(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 100,
+            "topk": [50],
+            "metrics": ["NDCG"],
+            "valid_metric": "NDCG@50",
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
