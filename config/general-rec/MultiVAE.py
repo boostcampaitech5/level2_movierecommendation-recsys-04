287be5fdbc8788c8333238f786e2751b7ff39c33
임도현_T5170 <86715604+limstonestone@users.noreply.github.com>
2023-06-08 10:49:49 +0900

    feat : add Multi-VAE configs & fix infrence format
    * fix : modify inference
    
    * refactor : apply Black Formatter
    
    * feat : add Multi-VAE configs

--- config/general-rec/MultiVAE.py
+++ config/general-rec/MultiVAE.py
@@ -129,6 +129,102 @@ class Ver0_0_5(BaseConfig):
                 "mode": "full",
             },
         }
-        self.parameter_dict = dict(
-            self.base_parameter_dict, **self.parameter_dict
-        )
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_6(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 2,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "train_batch_size": 256,  # 2048 -> 256
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_7(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 200,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "dropout_prob": 0.1,  # 0.5 -> 0.1
+            "stopping_step": 20,  # 10 -> 20
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_8(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 100,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "seed": 42,  # 2020 -> 42
+            "stopping_step": 20,  # 10 -> 20
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_9(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 200,
+            "topk": [10],
+            "eval_args": {
+                "split": {"RS": [0.8, 0.1, 0.1]},
+                "order": "TO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "stopping_step": 20,  # 10 -> 20
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
+
+
+class Ver0_0_10(BaseConfig):
+    def __init__(self):
+        super().__init__()
+        self.parameter_dict = {
+            "load_col": {"inter": ["user", "item", "time"]},
+            "neg_sampling": None,
+            "epochs": 500,
+            "topk": [10],
+            "eval_args": {
+                "split": {"LS": "valid_and_test"},
+                "order": "RO",
+                "group_by": "user",
+                "mode": "full",
+            },
+            "learning_rate": 0.0001,  # 0.001 -> 0.0001
+            "stopping_step": 20,  # 10 -> 20
+        }
+        self.parameter_dict = dict(self.base_parameter_dict, **self.parameter_dict)
