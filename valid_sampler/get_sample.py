db44eeeeb671cac712a9c7f65c2d93bdc8bb2142
sumin <ksum.jerry@gmail.com>
2023-06-23 11:38:17 +0900

    feat : update valid_sampler
    '#50'

--- valid_sampler/get_sample.py
+++ valid_sampler/get_sample.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 
 import argparse
+from sampler import load_data, sampling
 
 
 def main(args):
     print("##### Load data ...")
-    sota_top100 = pd.read_csv("./submissions/HighOrderEASE_top100.csv")
+    inters, _, _ = load_data()
     print("##### Load data done!")
 
     np.random.seed(args.start_seed)
@@ -21,30 +22,10 @@ def main(args):
     else:
         print("##### Something happens wrong...")
 
-    valid_set = sampling(seed, sota_top100)
+    valid_set = sampling(seed, inters)
 
     os.makedirs("./output", exist_ok=True)
     valid_set.to_csv(f"./output/valid_set.csv", index=False)
-
-
-def sampling(seed, sota, k=10):
-    np.random.seed(seed)
-
-    # 그룹화된 데이터프레임 생성
-    grouped = sota.groupby("user")
-
-    # 각 그룹에서 임의의 인덱스 선택
-    shuffled_indices = grouped.apply(
-        lambda x: np.random.choice(x.index, k, replace=False)
-    )
-
-    # 선택된 인덱스를 사용하여 해당 행을 가져옴
-    result = sota.loc[shuffled_indices.explode()]
-
-    # 결과 데이터프레임 생성
-    result = result.reset_index(drop=True)
-
-    return result
 
 
 if __name__ == "__main__":
